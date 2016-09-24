b78839ddeecee0df262d752d0eda2c1ee31eefa2
DanielSeton <danielseton@live.nl>
2016-09-24 11:14:12 +0200

    Add files via upload

---
+++ Les3/ParticleEmmitter.cpp
@@ -0,0 +1,25 @@
+#include "ParticleEmmitter.h"
+
+
+
+void ParticleEmmitter::setup()
+{
+	for (int i = 0; i < particles.size(); i++) 
+	{
+		partl = Particle();
+	}
+}
+
+void ParticleEmmitter::update()
+{
+	for (int i = 0; i < particles.size(); i++) {
+		particles[i].move();
+	}
+}
+
+void ParticleEmmitter::draw()
+{
+	for (int i = 0; i < particles.size(); i++) {
+		particles[i].draw();
+	}
+}
---
+++ Les3/ParticleEmmitter.h
@@ -0,0 +1,16 @@
+#include "ofMain.h"
+#include "Particle.h"
+
+
+class ParticleEmmitter
+{
+public:
+	void setup();
+	void update();
+	void draw();
+
+	Particle partl;
+
+	array<Particle, 100> particles;
+};
+
