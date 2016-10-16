cd899d80c9565295a6cd52f940fe5d96a163de92
DanielSeton <danielseton@live.nl>
2016-10-16 20:36:21 +0200

    Add files via upload

---
+++ Les%206/Les 6/src/Particle.h
@@ -0,0 +1,33 @@
+#pragma once
+
+
+#include "ofMain.h"
+
+class Particle {
+public:
+	Particle(int startX, int startY, ofColor inner, ofColor outer);
+
+	virtual void move();
+
+	void draw();
+
+	void update();
+
+	bool IsDead();
+
+	void setColours(ofColor center, ofColor inner, ofColor outer);
+
+	int lifetime;
+
+protected:
+	ofPoint position;
+	float radius;
+	ofVec2f speed;
+
+
+	ofColor centerColour = ofColor::white;
+	ofColor innerColour;
+	ofColor outerColour;
+
+};
+
---
+++ Les%206/Les 6/src/ParticleNieuw.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "ofMain.h"
+#include "Particle.h"
+
+class ParticleNieuw : public Particle
+{
+public:
+	ParticleNieuw(int startX, int startY, ofColor inner, ofColor outer);
+
+	virtual void setColours(ofColor center, ofColor inner, ofColor outer);
+
+};
+
